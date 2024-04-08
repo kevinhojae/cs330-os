0c3823f0e753c8488288c822af13465f79f37348
kevinhojae <kevinhojae@kaist.ac.kr>
2024-04-08 21:07:28 +0000

    fix: fix run script

--- userprog/scripts/run.sh
+++ userprog/scripts/run.sh
@@ -7,7 +7,7 @@
 # e.g. bash ./scripts/test.sh -t args-multiple -c 'args-multiple onearg twoarg threearg' -p ../../userprog/build/tests/userprog/args-multiple -p ../../userprog/build/tests/userprog/args-multiple -p ../../userprog/build/tests/userprog/args-multiple
 
 # get arguments from command line
-while getopts "m:t:c:p:" opt; do
+while getopts "m:t:c:f:p:" opt; do
     case ${opt} in
     m)
         mode=$OPTARG
@@ -18,8 +18,11 @@ while getopts "m:t:c:p:" opt; do
     c)
         command=$OPTARG
         ;;
-    p)
+    f)
         put_files="-p ../../tests/userprog/$OPTARG:$OPTARG"
+        ;;
+    p)
+        put_codes="-p tests/userprog/$OPTARG:$OPTARG"
         ;;
     \?)
         echo "Usage: bash scripts/test.sh -t test -c command"
@@ -28,6 +31,9 @@ while getopts "m:t:c:p:" opt; do
 done
 
 # rebuild
+cd /root/cs330-os/userprog/build
+make clean
+
 cd /root/cs330-os/userprog
 make clean
 make
@@ -38,12 +44,12 @@ pintos-mkdisk filesys.dsk 10
 
 # if mode is test, then run the test
 if [ "$mode" = "test" ]; then
-    echo "pintos --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} -- -q -f run \"${command}\""
-    pintos --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} -- -q -f run "${command}"
+    echo "pintos --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} ${put_codes} -- -q -f run \"${command}\""
+    pintos --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} ${put_codes} -- -q -f run "${command}"
 fi
 
 # if mode is debug, then run the test with gdb
 if [ "$mode" = "debug" ]; then
-    echo "pintos --gdb --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} -- -q -f run \"${command}\""
-    pintos --gdb --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} -- -q -f run "${command}"
+    echo "pintos --gdb --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} ${put_codes} -- -q -f run \"${command}\""
+    pintos --gdb --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files} ${put_codes} -- -q -f run "${command}"
 fi
