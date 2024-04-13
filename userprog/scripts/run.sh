64bf9cee914074b44d4cb8ddbd2739cc7c66067d
kevinhojae <kevinhojae@kaist.ac.kr>
2024-04-13 08:34:46 +0000

    feat: update run script to support multi-oom

--- userprog/scripts/run.sh
+++ userprog/scripts/run.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-# bash ./scripts/test.sh -t args-single -c 'args-single onearg'
-# pintos --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
-
-# take put arguments as list
-# e.g. bash ./scripts/test.sh -t args-multiple -c 'args-multiple onearg twoarg threearg' -p ../../userprog/build/tests/userprog/args-multiple -p ../../userprog/build/tests/userprog/args-multiple -p ../../userprog/build/tests/userprog/args-multiple
-
 # get arguments from command line
-while getopts "m:t:c:f:p:" opt; do
-    case ${opt} in
-    m)
-        mode=$OPTARG
+while [ "$1" != "" ]; do
+    case $1 in
+    -m | --mode)
+        shift
+        mode=$1
         ;;
-    t)
-        test=$OPTARG
+    -t | --test)
+        shift
+        test=$1
         ;;
-    c)
-        command=$OPTARG
+    -c | --command)
+        shift
+        command=$1
         ;;
-    f)
-        put_files="-p ../../tests/userprog/$OPTARG:$OPTARG"
+    --pc | --put_code)
+        shift
+        put_codes="-p tests/userprog/$1:$1"
         ;;
-    p)
-        put_codes="-p tests/userprog/$OPTARG:$OPTARG"
+    --pf | --put_file)
+        shift
+        put_files="-p ../../tests/userprog/$1:$1"
         ;;
-    \?)
-        echo "Usage: bash scripts/test.sh -t test -c command"
+    --novm | --novm_folder)
+        shift
+        novm_folder=$1
         ;;
     esac
+    shift
 done
+
+# if vm, then put the code from the vm folder, tests/userprog/vm/$1:$1
+if [ "$novm_folder" = "true" ]; then
+    put_test="-p tests/userprog/no-vm/${test}:${test}"
+else
+    put_test="-p tests/userprog/${test}:${test}"
+fi
 
 # rebuild
 cd /root/cs330-os/userprog/build
@@ -44,12 +52,12 @@ pintos-mkdisk filesys.dsk 10
 
 # if mode is test, then run the test
 if [ "$mode" = "test" ]; then
-    echo "pintos --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files}${put_codes} -- -q -f run \"${command}\""
-    pintos --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files}${put_codes} -- -q -f run "${command}"
+    echo "pintos --fs-disk filesys.dsk ${put_test} ${put_files} ${put_codes} -- -q -f run \"${command}\""
+    pintos --fs-disk filesys.dsk ${put_test} ${put_files} ${put_codes} -- -q -f run "${command}"
 fi
 
 # if mode is debug, then run the test with gdb
 if [ "$mode" = "debug" ]; then
-    echo "pintos --gdb --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files}${put_codes} -- -q -f run \"${command}\""
-    pintos --gdb --fs-disk filesys.dsk -p tests/userprog/${test}:${test} ${put_files}${put_codes} -- -q -f run "${command}"
+    echo "pintos --gdb --fs-disk filesys.dsk ${put_test} ${put_files} ${put_codes} -- -q -f run \"${command}\""
+    pintos --gdb --fs-disk filesys.dsk ${put_test} ${put_files} ${put_codes} -- -q -f run "${command}"
 fi
