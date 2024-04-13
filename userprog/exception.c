306229a935ec82a3d9f41ceb096f9b0f2ea99314
kevinhojae <kevinhojae@kaist.ac.kr>
2024-04-13 12:14:34 +0000

    fix: fix exception handling condition

--- userprog/exception.c
+++ userprog/exception.c
@@ -141,7 +141,7 @@ page_fault (struct intr_frame *f) {
 	write = (f->error_code & PF_W) != 0;
 	user = (f->error_code & PF_U) != 0;
 
-	if (!not_present || !write || !user) {
+	if (not_present || !write || user) {
 		exit_handler (-1);
 	}
 
