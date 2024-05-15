0228918b0d1d7e35d79a84c7a82cbd4a25c09b43
kyj <k45824@kaist.ac.kr>
2024-05-15 08:07:02 +0000

    fix: uninit_destroy

--- vm/uninit.c
+++ vm/uninit.c
@@ -63,17 +63,11 @@ uninit_initialize (struct page *page, void *kva) {
  * PAGE will be freed by the caller. */
 static void
 uninit_destroy (struct page *page) {
-	struct uninit_page *uninit UNUSED = &page->uninit;
+	struct uninit_page *uninit = &page->uninit;
 	/* TODO: Fill this function.
 	 * TODO: If you don't have anything to do, just return. */
 
-	// check the vm type of the page and free the resource accordingly
-	// if(uninit->type == VM_ANON){
-	// 	// free the resource of anon page
-	// 	anon_destroy (page);
-	// 	free (page);
-	// }
+	page_free_fun(uninit->aux);
 	
-	// TODO: handle rest of the types
 	return;
 }
